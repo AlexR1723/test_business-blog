6d9fed823760c26f0c9a79bbd015df6994889fb8
Alex <avdeenko.aleksey@yandex.com>
2019-09-06 18:27:58 +0300

    2

--- test_business_blog/settings.py
+++ test_business_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')dps2l_-v8c^080_8vm_v(m%0-^k#e-2)0dnw+0w@d$@((#be1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

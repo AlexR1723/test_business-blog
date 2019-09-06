2a5077b97fe54c7f46b9dee7c75cf01301009a7e
Alex <avdeenko.aleksey@yandex.com>
2019-09-06 22:38:49 +0300

    settings 9

--- test_business_blog/settings.py
+++ test_business_blog/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'test_business_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'',
-        'USER': '',
-        'PASSWORD':'',
+        'NAME':'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'ppass1723',
         'HOST':'localhost',
         'PORT':'5432'
     }

7fdba9575d6c5f27febe643cb18da4a565bc72e3
avdeenko.aleksey@yandex.com <grinya22041982>
2019-09-15 14:37:54 +0300

    final 4

--- test_business_blog/settings.py
+++ test_business_blog/settings.py
@@ -143,14 +143,3 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_URL = '/static/'
 #STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
-
-# Extra lookup directories for collectstatic to find static files
-#django_heroku.settings(locals())
-
-#  Add configuration for static files storage using whitenoise
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-#import dj_database_url
-#prod_db  =  dj_database_url.config(conn_max_age=500)
-#DATABASES['postgresql_psycopg2'].update(prod_db)
